18ff38c7701071f66158b66d5ef803ed201ce9a2
Andrzej Ładomirski <ladomirski@gmail.com>
2017-03-22 18:41:29 +0100

    active body map

--- scripts/bodyMap.js
+++ scripts/bodyMap.js
@@ -6,4 +6,33 @@ $(document).ready(function() {
 		var href=$(this).attr("href");
 		$(href).fadeOut(200);
 	});
+
+	$("#breast-area").click(function() {
+		$('.treatments-text').hide();
+		$('#breast-text').fadeToggle(2000);
+	});
+	$("#hair-area").click(function() {
+		$('.treatments-text').hide();
+		$('#hair-text').fadeToggle(2000);
+	});
+	$("#hands-area").click(function() {
+		$('.treatments-text').hide();
+		$('#hands-text').fadeToggle(2000);
+	});
+	$("#face-area").click(function() {
+		$('.treatments-text').hide();
+		$('#face-text').fadeToggle(2000);
+	});
+	$("#neck-area").click(function() {
+		$('.treatments-text').hide();
+		$('#neck-text').fadeToggle(2000);
+	});
+	$("#legs-area").click(function() {
+		$('.treatments-text').hide();
+		$('#legs-text').fadeToggle(2000);
+	});
+	$("#torse-area").click(function() {
+		$('.treatments-text').hide();
+		$('#torse-text').fadeToggle(2000);
+	});
 });
