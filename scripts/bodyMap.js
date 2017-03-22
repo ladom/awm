c58097d412a4af8be424434ba0d9c728494502a3
Andrzej Ładomirski <ladomirski@gmail.com>
2017-03-22 21:16:58 +0100

    added preventDefault function

--- scripts/bodyMap.js
+++ scripts/bodyMap.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	$("area").hover(function() {
 		var href=$(this).attr("href");
 		$(href).fadeIn(100);
@@ -7,31 +8,38 @@ $(document).ready(function() {
 		$(href).fadeOut(200);
 	});
 
-	$("#breast-area").click(function() {
+	$("#breast-area").click(function(e) {
+		e.preventDefault();
 		$('.treatments-text').hide();
 		$('#breast-text').fadeToggle(2000);
 	});
-	$("#hair-area").click(function() {
+	$("#hair-area").click(function(e) {
+		e.preventDefault();
 		$('.treatments-text').hide();
 		$('#hair-text').fadeToggle(2000);
 	});
-	$("#hands-area").click(function() {
+	$("#hands-area").click(function(e) {
+		e.preventDefault();
 		$('.treatments-text').hide();
 		$('#hands-text').fadeToggle(2000);
 	});
-	$("#face-area").click(function() {
+	$("#face-area").click(function(e) {
+		e.preventDefault();
 		$('.treatments-text').hide();
 		$('#face-text').fadeToggle(2000);
 	});
-	$("#neck-area").click(function() {
+	$("#neck-area").click(function(e) {
+		e.preventDefault();
 		$('.treatments-text').hide();
 		$('#neck-text').fadeToggle(2000);
 	});
-	$("#legs-area").click(function() {
+	$("#legs-area").click(function(e) {
+		e.preventDefault();
 		$('.treatments-text').hide();
 		$('#legs-text').fadeToggle(2000);
 	});
-	$("#torse-area").click(function() {
+	$("#torse-area").click(function(e) {
+		e.preventDefault();
 		$('.treatments-text').hide();
 		$('#torse-text').fadeToggle(2000);
 	});
